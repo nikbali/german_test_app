93a1426b3cb9991a1c085588d16afc279a999182
nbalily <nikbali@mail.ru>
2018-05-27 20:25:17 +0300

    Доработал обработку исключений для GET Query запроса

--- src/main/java/servlets/QuestionsController.java
+++ src/main/java/servlets/QuestionsController.java
@@ -2,11 +2,12 @@ package servlets;
 
 import com.google.gson.Gson;
 
+import com.google.gson.JsonElement;
 import dao.DAOFactory;
 import dao.GenericDAO;
 import dao.MySqlDaoFactory;
+import model.Error;
 import model.Question;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,28 +23,35 @@ public class QuestionsController extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
     {
+        Gson gson = new Gson();
+        String json = "";
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(res.getOutputStream()));
         try
         {
             int id =Integer.parseInt(req.getParameter("id"));
             MySqlDaoFactory dao = MySqlDaoFactory.getInstance();
             GenericDAO<Question> question_dao = dao.getQuestionDAO();
             Question question = question_dao.getByPK(id);
-            Gson gson = new Gson();
-            String stringJson = gson.toJson(question);
-            System.out.println(stringJson);
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(res.getOutputStream()));
-            writer.write(stringJson);
-            writer.flush();
-            writer.close();
-        }
-        catch (SQLException sql_ex)
-        {
-           sql_ex.getMessage();
+            json = gson.toJson(question);
+            System.out.println("Succes Query: "+ json);
 
+        }
+        catch (SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+            res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            json = gson.toJson(new Error("The resource you requested could not be found.", false));
         }
         catch (NumberFormatException ex)
         {
-            ex.getMessage();
+            System.out.println(ex.getMessage());
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            json = gson.toJson(new Error("Incorrect request parameters.", false));
+        }
+        finally {
+            writer.write(json);
+            writer.flush();
+            writer.close();
         }
 
     }
