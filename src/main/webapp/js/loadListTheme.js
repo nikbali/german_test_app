6ad54b979d668e3d8fc052131ce2e94ac4fd8e57
nbalily <nikbali@mail.ru>
2018-06-13 14:19:49 +0300

    js для загрузки списка тематик и создания панелей с ответами

---
+++ src/main/webapp/js/loadListTheme.js
@@ -0,0 +1,41 @@
+var xhr = new XMLHttpRequest();
+var object_test;
+var thematics = [];
+butAdd = document.getElementById('btn_theme');
+
+function loadListTheme(){
+try
+{
+   
+      var url = "/querytheme?test_id=1";
+      xhr.open('GET', url);
+     xhr.onload = function (e) 
+	 {
+		  if (xhr.status == 200)
+		  { 
+				object_test = JSON.parse(xhr.responseText);
+				thematics = object_test.thematics;
+				
+				var list = document.getElementById('list_theme');
+				// очистка списка
+				while (list.firstChild) 
+				{
+				list.removeChild(list.firstChild);
+				}           
+				// заполнение списка
+				for (var i = 0; i < thematics.length; i++)
+				{
+					var element = document.createElement('li');
+					element.innerHTML = '<a href="#">'+thematics[i].name +'</a>';
+					list.appendChild(element);           
+				} 					
+		  }
+	 };	  
+	xhr.send(null);
+}
+catch(e)
+{
+    alert('Error load list thematics'); 
+    document.location.href ='index.html';
+}
+}
